29462f55fbf1631d61fa056a65b1b9abe5e93f28
chillpert <dev@christianhilpert.com>
2021-04-13 21:21:03 +0200

    even more rust fun

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,14 @@ fn main() {
     // $ cargo build // builds the project
     // $ cargo run // also builds the project
 
+    // How to compile in release mode:
+    // $ rustc main.rs --release
+    // This will cause integer overflow checks to be disabled and two complement's wrapping is used
+    // This means, given a u8 (0 - 255):
+    // 256 -> 0
+    // 257 -> 1
+    // Relying on this behaviour is considered an error
+
     // _ (underscore) in front of variable name will supress warning that the variable is not used
 
     // bool
@@ -77,6 +85,10 @@ fn main() {
         let mut _mut_a = 0.0;
         _mut_a = 1337.0;
 
+        // assigning a mutable to a non mutable is possible
+        // thus, this is not a true constant (see below for actual constants)
+        let _b = _mut_a;
+
         // mut_a = 'd'; // illegal: mismatched types
         // mut_a = 213; // illegal: mismatched types
     }
@@ -138,22 +150,47 @@ fn main() {
 
     // functions
     {
+        // evaluations vs. expressions
         function_a();
     }
 
-    // How to compile in release mode:
-    // $ rustc main.rs --release
-    // This will cause integer overflow checks to be disabled and two complement's wrapping is used
-    // This means, given a u8 (0 - 255):
-    // 256 -> 0
-    // 257 -> 1
-    // Relying on this behaviour is considered an error
+    // const
+    {
+        // replace let with const
+        // always write const variables in upper case
+        // you must always annotate the type
+        // the main difference between normal variables is that they can
+        // only be assigned constant expressions and not the output of a function
+        // or anything else that is calculated during runtime
+        const _MAX_POINT: u32 = 100_00;
+    }
+
+    // shadowing
+    {
+        // this is valid in rust and called shadowing
+        // note how none of these were declared as mutable (mut)
+        // you would get a compile-time error if you would not use let
+        // this is useful if you have a case where a variable is only changed once
+        // because you can just shadow it and it will continue being immutable again
+        let x = 5;
+        let x = x + 1;
+        let x = x + 2; // x = 8
+
+        println!("The value of x is: {}", x);
+
+        // you can also use it to change a variable's type and reuse the same name
+        let _spaces = "   ";
+        let _spaces = _spaces.len();
+    }
 }
 
-
-fn function_a( ) {
+fn function_a() {
     let _y = {
         let _x = 3;
+        // no semicolon because this is an evaluation
+        // if it had one then it would be an expression
+        // but you cannot assign an expression to an expression
+        // only evaluation can be assigned
         _x + 1
     };
 
